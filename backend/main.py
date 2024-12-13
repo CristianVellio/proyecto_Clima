99cf89e6a97f102f6aa88867d6915078a1604b6c
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-13 08:58:31 -0300

    ss

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://proyecto-clima-nywds8nnt-chris-projects-be539ae8.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
