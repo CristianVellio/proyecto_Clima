99e05e08c291cfc9c934dc87a07cc7b1d86038fe
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-14 00:51:31 -0300

    dwdw

--- backend/main.py
+++ backend/main.py
@@ -19,6 +19,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    allow_headers=["Content-Type", "Authorization"],
 )
 
 # Main route to check if the backend is working
