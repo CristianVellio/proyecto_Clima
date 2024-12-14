8c5336402bd758d91fe87333e0932c740ef60411
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-14 03:03:33 -0300

    htrhrt

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*", 
+        "http://localhost:5173",  # Local para desarrollo
+        "https://proyecto-clima-azure.vercel.app",  # URL de frontend en Vercel
     ],
     allow_credentials=True,
     allow_methods=["*"],
