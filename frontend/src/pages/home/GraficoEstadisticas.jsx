45a8338e60083404edc8db26df1cbabae97634ea
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-18 16:38:43 -0300

    test 3 tamaño

--- frontend/src/pages/home/GraficoEstadisticas.jsx
+++ frontend/src/pages/home/GraficoEstadisticas.jsx
@@ -114,8 +114,10 @@ const GraficoEstadisticas = () => {
       <h2 className="text-center text-2xl font-bold text-gray-800 mb-4">
         Estadísticas de Humedad y Temperatura
       </h2>
-      <div className="hidden md:block">
-        <Bar data={chartData} options={options} />
+      <div className="block">
+        <div className="md:w-[800px] md:h-[500px] w-full h-[300px] mx-auto">
+          <Bar data={chartData} options={options} />
+        </div>
       </div>
     </div>
   );
