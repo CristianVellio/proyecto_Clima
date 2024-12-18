0a1faf5699eac158a49e64d0bc217fc5eca2e292
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-18 17:36:20 -0300

    A ver

--- frontend/src/pages/home/Estadisticas.jsx
+++ frontend/src/pages/home/Estadisticas.jsx
@@ -1,4 +1,5 @@
 import GraficoEstadisticas from "./GraficoEstadisticas";
+import Irrigo2 from "../../assets/Irrigo2.png";
 
 const Estadisticas = () => {
   return (
@@ -9,15 +10,20 @@ const Estadisticas = () => {
       </h2>
 
       {/* Contenido principal: Gráfico y Descripción */}
-      <div className="md:flex md:items-center md:justify-between gap-12 w-full">
+      <div className="md:flex md:items-start md:justify-between gap-8 w-full max-w-7xl">
         {/* Contenedor del gráfico */}
-        <div className="md:w-1/2 w-full">
+        <div className="md:w-2/3 w-full">
           <GraficoEstadisticas />
         </div>
 
-        {/* Contenedor del párrafo */}
-        <div className="md:w-1/2 w-full">
-          <p className="text-lg text-gray-700">
+        {/* Contenedor del párrafo y la imagen */}
+        <div className="md:w-1/3 w-full flex flex-col gap-6 items-start">
+          <img
+            src={Irrigo2}
+            alt="Irrigo"
+            className="rounded-md w-full object-cover h-40 md:h-60"
+          />
+          <p className="text-lg text-gray-700 leading-relaxed">
             El gráfico de estadísticas es un diagrama de barras diseñado para
             visualizar los valores de temperatura (en grados Celsius) y humedad
             (en porcentaje) recopilados de las mediciones de un sistema de
