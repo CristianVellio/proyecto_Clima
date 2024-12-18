b1fa4d43f50da1d8f657d39e3292297004c435b5
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-12-18 16:32:33 -0300

    test 1

--- frontend/src/pages/home/Estadisticas.jsx
+++ frontend/src/pages/home/Estadisticas.jsx
@@ -2,16 +2,22 @@ import GraficoEstadisticas from "./GraficoEstadisticas";
 
 const Estadisticas = () => {
   return (
-    <div className="flex md:flex py-16 justify-between items-center gap-12">
-      <div className="md:w-1/2 w-full">
-        <h2 className="md:text-5xl text-2xl font-medium mb-7">
-          Tablero de Estadisticas
-        </h2>
-        <div>
+    <div className="flex flex-col py-16 items-center gap-12">
+      {/* Título centrado */}
+      <h2 className="text-center md:text-5xl text-2xl font-medium mb-7">
+        Tablero de Estadísticas
+      </h2>
+
+      {/* Contenido principal: Gráfico y Descripción */}
+      <div className="md:flex md:items-center md:justify-between gap-12 w-full">
+        {/* Contenedor del gráfico */}
+        <div className="md:w-1/2 w-full">
           <GraficoEstadisticas />
         </div>
-        <div>
-          <p>
+
+        {/* Contenedor del párrafo */}
+        <div className="md:w-1/2 w-full">
+          <p className="text-lg text-gray-700">
             El gráfico de estadísticas es un diagrama de barras diseñado para
             visualizar los valores de temperatura (en grados Celsius) y humedad
             (en porcentaje) recopilados de las mediciones de un sistema de
